bc35b2885f670fe288fbb49ff881b936f3969e70
Sam Choi <sam.choi@berkeley.edu>
2016-10-13 04:27:25 -0700

    nltk corpora and multi-word training implemented

--- autocomplete.py
+++ autocomplete.py
@@ -1,8 +1,12 @@
 import random
 import nltk
-from nltk.corpus import gutenberg
-# Spell Check and Auto-Complete
+from nltk.corpus import webtext
+# Spell Check and Auto-Complete application
 # Author: Sam Choi
+# Auto-Complete:
+# - Uses a stochastic model to predict the most likely next word
+# - Implements a Markov Decision Process based on the last word used
+
 
 class Markov():
 	def __init__(self, data=None):
@@ -12,6 +16,8 @@ class Markov():
 		self.next_word_map = {}
 
 	def train_word(self, word, next_word):
+		word = word.lower()
+		next_word = next_word.lower()
 		word_dict = self.word_history.setdefault(word, {next_word : 0})
 		word_dict.setdefault(next_word, 0)
 		word_dict[next_word] += 1
@@ -23,7 +29,6 @@ class Markov():
 		if domain == 0:
 			domain = len(words)-1
 		for i in range(domain):
-			print(i)
 			self.train_word(words[i], words[i+1])
 
 	def update(self):
@@ -42,28 +47,27 @@ class Markov():
 
 # returns the next most probable word given a most recent word
 def auto_complete(model, query):
-	model.retrieve_next(query)
+	return model.retrieve_next(query)
 
 
 
 # test = Markov()
 # test.train_word("hello", "world")
-# test.train_word("hello", "friend")
 # test.train_word("hello", "world")
-# print(test.retrieve_next("hello"))
-
 # test.train_word("hello", "friend")
 # test.train_word("hello", "friend")
-# test.train_word("hello", "friend")
-# print(test.retrieve_next("hello"))
+# print(auto_complete(test, "hello"))
 
+# test.train_set("hello friend my name is Sam Choi hello friend hello friend".split(" "))
+# print(auto_complete(test, "hello"))
 
-# alice = gutenberg.words('carroll-alice.txt')
+new_york = webtext.words('overheard.txt')
+pirates = webtext.words('pirates.txt')
 
-# file_test = Markov()
-# file_test.train_set(alice, 10000)
-# print(file_test.word_history['go'])
-# print(file_test.retrieve_next('go'))
+file_test = Markov()
+file_test.train_set(pirates, 3000)
+print(file_test.word_history['what'])
+print(file_test.retrieve_next('what'))
 
 
 
